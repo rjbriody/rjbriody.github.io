cd234a8da96e13a2241b2e18c0e75224241e75e2
bobb <rjbriody@users.noreply.github.com>
2018-10-12 16:03:38 -0700

    fixing styling

--- site-project/src/App.js
+++ site-project/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'react-emotion';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 
 import theme from './theme.js';
 import Header from './Header.js';
@@ -10,6 +13,13 @@ import Intro from './sections/intro/Intro.js';
 import Skills from './sections/skills/Skills.js';
 import Tech from './sections/tech/Tech.js';
 import Testimonials from './sections/testimonials/Testimonials.js';
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  insertionPoint: document.getElementById('jss-insertion-point'),
+});
 
 const
   AppBarSpacer = styled('div')`
@@ -20,16 +30,20 @@ const
     background-color: black;
   `;
 
-export default () => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <Header />
-    <AppBarSpacer />
-    <Intro />
-    <Testimonials />
-    <About />
-    <Skills />
-    <Tech />
-    <Footer />
-  </MuiThemeProvider>
+const App = () => (
+  <JssProvider jss={jss} generateClassName={generateClassName}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <AppBarSpacer />
+      <Intro />
+      <Testimonials />
+      <About />
+      <Skills />
+      <Tech />
+      <Footer />
+    </MuiThemeProvider>
+  </JssProvider>
 );
+
+export default App;
